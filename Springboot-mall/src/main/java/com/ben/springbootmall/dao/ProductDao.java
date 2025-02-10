56ec3b21a71d92f63f41dcc19bea034f0bb5ba66
Ben Lin <abenq777@gmail.com>
2025-02-10 15:56:17 +0800

    商品功能-實做新增商品

--- Springboot-mall/src/main/java/com/ben/springbootmall/dao/ProductDao.java
+++ Springboot-mall/src/main/java/com/ben/springbootmall/dao/ProductDao.java
@@ -1,7 +1,11 @@
 package com.ben.springbootmall.dao;
 
+import com.ben.springbootmall.dto.ProductRequest;
 import com.ben.springbootmall.model.Product;
 
 public interface ProductDao {
-    Product getProductById(Integer productId);
+
+    Product getProductById(Integer ProductId);
+
+    Integer createProduct(ProductRequest productRequest);
 }
--- Springboot-mall/src/main/java/com/ben/springbootmall/service/ProductService.java
+++ Springboot-mall/src/main/java/com/ben/springbootmall/service/ProductService.java
@@ -1,7 +1,10 @@
 package com.ben.springbootmall.service;
 
+import com.ben.springbootmall.dto.ProductRequest;
 import com.ben.springbootmall.model.Product;
 
 public interface ProductService {
     Product getProductById(Integer productId);
+
+    Integer createProduct(ProductRequest productRequest);
 }
